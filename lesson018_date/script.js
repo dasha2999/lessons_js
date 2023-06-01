70d5909f7ad5b05926e026b549dd25192be1bfbb
dasha2999 <125147552+dasha2999@users.noreply.github.com>
2023-06-01 19:08:22 +0300

    Add files via upload

---
+++ lesson018_date/script.js
@@ -0,0 +1,30 @@
+// date - дата
+
+// const date = new Date()
+// console.log(date)
+
+// const date = new Date()
+// new Date(year, month, date, hours, minutes, seconds, ms)
+
+// console.log(date.toLocaleDateString()) // - дата
+// console.log(date.toLocaleTimeString()) // - время
+// console.log(date.toLocaleString()) // - дата и время через запятую
+
+// console.log(date.getFullYear()) // - текущий год
+// console.log(date.getMonth()) // - 0 это январь, 11 это декабрь
+// console.log(date.getDate()) // - число
+// console.log(date.getDay()) // - день недели, 1 это понедельник, 6 это суббота и 0 это воскресенье
+// console.log(date.getHours()) // - часы
+// console.log(date.getMinutes()) // - минуты
+// console.log(date.getSeconds()) // - секунды
+// console.log(date.getMilliseconds()) // - миллисекунды
+
+// console.log(date.getTime()) // - сколько прошло времени c 1 января 1970 года в миллисекундах
+
+// const date = Date.now()
+// console.log(date) // - getTime
+
+// date.set... - для установки времени, года и т.д
+
+// date.setFullYear(2022)
+// console.log(date) // - 2022
