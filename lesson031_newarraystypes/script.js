14c0319a7986fda03e54ad30edb59ea27188f605
dasha2999 <125147552+dasha2999@users.noreply.github.com>
2023-07-20 19:09:15 +0300

    Add files via upload

---
+++ lesson031_newarraystypes/script.js
@@ -0,0 +1,36 @@
+const numbers = [2,5,9,7,1]
+
+// numbers[1] = 20
+
+// with()
+// const newArray = numbers.with(1,29)
+
+// console.log('исходный', numbers)
+// console.log('новый', newArray)
+
+
+// numbers.sort()
+
+// toSorted()
+// const sortedArray = numbers.toSorted()
+
+// console.log('исходный', numbers)
+// console.log('toSorted', sortedArray)
+
+
+// numbers.toreverse()
+
+// toResersed()
+// const reversedArray = numbers.toReversed()
+
+// console.log('исходный', numbers)
+// console.log('toReversed', reversedArray)
+
+
+// toSpliced()
+// const splicedArray = numbers.toSpliced(1,0,10)
+
+// console.log('исходный', numbers)
+// console.log('toSpliced', splicedArray)
+
+
