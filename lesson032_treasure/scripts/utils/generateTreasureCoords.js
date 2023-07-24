5c3ce5ddb511a69972d10c313f4c1d62915d6d56
dasha2999 <125147552+dasha2999@users.noreply.github.com>
2023-07-24 18:32:32 +0300

    Delete lesson032_treasure directory

--- lesson032_treasure/scripts/utils/generateTreasureCoords.js
+++
@@ -1,6 +0,0 @@
-import { generateRandomNumbers } from "./generateRandomNumbers.js"
-
-export const generateTreasureCoords = ({width, height}, gap) => ({
-     x: generateRandomNumbers(width, gap),
-     y: generateRandomNumbers(height, gap)
-})
